02f8021ba0ee7d1dc7780779d3c8a8db3ed8b23c
SHAMI_TOMITA <shamitomita@gmail.com>
2023-07-18 16:53:43 -0400

    enum roles for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
+
+  enum :role, attendee: 0, admin: 1
 end
