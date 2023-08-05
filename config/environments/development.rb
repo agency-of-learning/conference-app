4a7219fbc51fe5516c46e837d3550f96fe9753fa
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-05 18:52:14 -0400

    dev local host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   
   config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 end
