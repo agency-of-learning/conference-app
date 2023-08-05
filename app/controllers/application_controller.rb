0f1842a49c7c3b0ce43a532afc0ccff8a3e47921
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-05 14:26:07 -0400

    stash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:receive_app_notifications, :receive_email_notifications])
-  end
 end
