6d1282f1d717e8a031bb65ba3d7ad3c1d6de8f9f
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-07 12:28:38 -0400

    notifications update read methods

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,17 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.unread
   end
+
+  def read(notification, talk)
+    notification.update(read_at: Time.current)
+    redirect_to talk_path(talk)
+  end 
+
+  def read_all
+    current_user.notifications.map {|n| n.update(read_at: Time.current)}
+    redirect_to user_notifications_path
+  end 
+
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,36 @@ class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
 
+  scope :unread, -> { where(read_at: nil).order(created_at: :desc) }
+
+  after_create_commit -> { broadcast_prepend_to "notifications"}
+  #after_update_commit -> { broadcast_replace_to "notifications"}
+  after_create_commit do
+    broadcast_replace_to "broadcast_to_user_#{self.recipient_id}", 
+      target: "notifications_count", 
+      partial: "notifications/count", 
+      locals: { count: self.recipient.unread_notifications_count }
+  end
+
+  after_create_commit do
+    broadcast_replace_to "mobile", 
+      target: "notifications_count_mobile", 
+      partial: "notifications/count_mobile", 
+      locals: { count: self.recipient.unread_notifications_count }
+  end
+
+  def talk_title
+    if self[:params][:talk]
+      self[:params][:talk][:title]
+    end 
+  end 
+
+  def talk_start_time 
+     if self[:params][:talk]
+      self[:params][:talk][:start_time].strftime("%I:%M %p, %a %d, %b %Y")
+     end 
+  end 
+
   def talk 
     self[:params][:talk]
   end 
