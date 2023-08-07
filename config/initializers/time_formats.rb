90cdc110e8227a57dc020e91e93dad6fc8aa1dfd
Eric Halverson <ericleifhalverson@gmail.com>
2023-08-07 16:08:25 -0700

    Modify '_talk' partial and 'talk#show' pages to better match katya's designs. Modify custom time_format to show full year.
    Fix bug in 'talks#show' page

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
-Time::DATE_FORMATS[:month_day_year] = "%B %-d, %y"
+Time::DATE_FORMATS[:month_day_year] = "%B %-d, %Y"
 Time::DATE_FORMATS[:standard_hour_minutes] = "%l:%M %P"
 Time::DATE_FORMATS[:twenty_four_hour_and_minutes] = "%R"
