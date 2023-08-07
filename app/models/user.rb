ecd3cbb5192225df0fddc8a185187bc5527600c2
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-07 12:29:05 -0400

    unread notifications count for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable
 
   has_many :notifications, as: :recipient
-  
   has_many :talks_users, dependent: :destroy, class_name: "TalkUser"
   has_many :talks, through: :talks_users
 
-
   enum :role, attendee: 0, admin: 1
+
+  def unread_notifications_count 
+    self.notifications.count {|n| !n.read_at}
+  end 
 end
