a782c8592ff71a44504eb53efce9b5dadeb3648c
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-08 18:44:16 -0400

    remove update notifications from talkmailer

--- app/mailers/talk_mailer.rb
+++ app/mailers/talk_mailer.rb
@@ -4,10 +4,6 @@ class TalkMailer < ApplicationMailer
 
   layout "mailer"
 
-  def update_for_talk
-    mail(subject: "#{@talk.title} has changed")
-  end
-
   def remind_for_upcoming_talk
     mail(subject: "#{@talk.title} is starting soon!")
   end
@@ -17,9 +13,4 @@ class TalkMailer < ApplicationMailer
   def set_talk
     @talk = params[:talk]
   end
-
-  #TODO: Remove if not going to use Noticed deliver_by: slack, this is for Talk::UpdateNotification
-  def set_changes 
-    @changes = params[:changes]
-  end 
 end
