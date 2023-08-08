84679cbd8f64fae1374a5e5e48628b65656363ea
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-08 18:42:48 -0400

    change

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -25,6 +25,7 @@ class TalksController < ApplicationController
   # POST /talks
   def create
     @talk = Talk.new(talk_params)
+    
     if @talk.save
       redirect_to @talk, notice: "Talk was successfully created."
     else
