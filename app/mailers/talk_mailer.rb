42499a2d572cf42bfcf804f7826c315006311141
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-08 19:26:59 -0400

    update

--- app/mailers/talk_mailer.rb
+++ app/mailers/talk_mailer.rb
@@ -1,12 +1,7 @@
 class TalkMailer < ApplicationMailer
-  before_action :set_talk, :set_changes
-  before_action :set_talk, :set_changes
+  before_action :set_talk
 
   layout "mailer"
-
-  def update_for_talk
-    mail(subject: "#{@talk.title} has changed")
-  end
 
   def remind_for_upcoming_talk
     mail(subject: "#{@talk.title} is starting soon!")
@@ -17,9 +12,4 @@ class TalkMailer < ApplicationMailer
   def set_talk
     @talk = params[:talk]
   end
-
-  #TODO: Remove if not going to use Noticed deliver_by: slack, this is for Talk::UpdateNotification
-  def set_changes 
-    @changes = params[:changes]
-  end 
 end
