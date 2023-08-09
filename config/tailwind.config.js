53d75f423474e0ccaa3858ba8f4e5c98ad357749
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-09 12:18:07 -0400

    navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,20 +14,6 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'red' : '#CC0000',
-      'orange' : '#FF654C',
-      'purple' : '#3B1D62',
-      'lilac' : '#A386FF',
-      'gray' : '#BDBDBD',
-      'light-grey' : '#F2F2F2',
-      'blue': '#3b82f6',
-      'pink': '#ec4899',
-      'green': '#22c55e'
-    }
   },
   plugins: [
     require('@tailwindcss/forms'),
