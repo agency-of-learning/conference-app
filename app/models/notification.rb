820606b8ebca63a0b7f376410856abcd16b6daf7
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-09 23:24:51 -0400

    additional broadcast and more stylings

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,7 +5,7 @@ class Notification < ApplicationRecord
   after_create_commit -> { broadcast_after_to "notifications", target: "notifications_display"}
   
   after_create_commit do
-    broadcast_replace_to "broadcast_to_user_#{self.recipient_id}", 
+    broadcast_update_to "broadcast_to_user_#{self.recipient_id}", 
       target: "notifications_count", 
       partial: "notifications/count", 
       locals: { count: self.recipient.unread_notifications_count }
@@ -14,8 +14,14 @@ class Notification < ApplicationRecord
   after_create_commit do
     broadcast_update_to "mobile", 
       target: "notifications_count_mobile", 
-      targets: "count",
       partial: "notifications/count_mobile", 
+      locals: { count: self.recipient.unread_notifications_count }
+  end
+
+  after_create_commit do
+    broadcast_update_to "menu", 
+      target: "notifications_count_menu", 
+      partial: "notifications/count_menu", 
       locals: { count: self.recipient.unread_notifications_count }
   end
 end
