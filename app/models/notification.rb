fea9b3d204c023fa9d15d0cd6b52a2c67c41aee8
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-10 11:23:23 -0400

    cosmetic changes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,8 +2,11 @@ class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
 
-  after_create_commit -> { broadcast_after_to "notifications", target: "notifications_display"}
-  
+  after_create_commit -> { broadcast_before_to "notifications", target: "notifications_display"}
+
+  #after_create_commit -> { broadcast_update_to "nu", target: "notifications_update"}
+  after_update_commit -> { broadcast_update_to "notifications_update", target: "notifications_update"}
+
   after_create_commit do
     broadcast_update_to "broadcast_to_user_#{self.recipient_id}", 
       target: "notifications_count", 
