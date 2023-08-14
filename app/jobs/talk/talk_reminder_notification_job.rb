470a6d60a8d040816a7cc0bd14a8ed3d959583f4
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-14 18:46:25 -0400

    guard clause

--- app/jobs/talk/talk_reminder_notification_job.rb
+++ app/jobs/talk/talk_reminder_notification_job.rb
@@ -2,8 +2,8 @@ class Talk::TalkReminderNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(user, talk)
-    if !user.notifications.any? {|n| n.type == "Talk::ReminderNotification" && n.to_notification.talk == talk}
-			Talk::ReminderNotification.with(talk: talk).deliver(user)
-		end 
+    return if !user.notifications.any? {|n| n.type == "Talk::ReminderNotification" && n.to_notification.talk == talk}
+		
+    Talk::ReminderNotification.with(talk: talk).deliver(user)
   end
 end
