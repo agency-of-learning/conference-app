c32b63bc4037bd2b1d6ad2a99b2cf27d6d947a13
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-14 14:14:21 -0400

    remove unneeded test_helper additions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,6 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require 'sidekiq/testing'
-
-
-Sidekiq::Testing.inline!
-Sidekiq::Testing.fake!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -13,6 +8,6 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  fixtures :notifications
+
   # Add more helper methods to be used by all tests here...
 end
