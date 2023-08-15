2c18d5a40213cdc3a52de3bb04cd979db31bbad9
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-15 13:45:14 -0400

    svg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def svg(name) #where should I go???
+        file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+        return File.read(file_path).html_safe if File.exist?(file_path)
+        '(not found)'
+    end
 end
