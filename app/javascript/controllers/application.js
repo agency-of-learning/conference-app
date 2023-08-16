928de44e2410ded2b0eb6406e58ff360d059ad5e
Cody Norman <wcodynorman@gmail.com>
2023-08-16 09:58:35 -0600

    register confetti controller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import { Confetti } from "stimulus-confetti"
+
+application.register('confetti', Confetti)
+
 const application = Application.start()
 
 import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
@@ -11,6 +15,8 @@ application.register('tabs', Tabs);
 application.register('popover', Popover);
 application.register('toggle', Toggle);
 application.register('slideover', Slideover);
+
+application.register('confetti', Confetti)
 
 // Configure Stimulus development experience
 application.debug = false
