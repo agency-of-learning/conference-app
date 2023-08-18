ea8449bb3e7759939a1d21e91eee5ac5b41ee0e0
Eric Halverson <ericleifhalverson@gmail.com>
2023-08-17 17:28:38 -0700

    Add 'uniqueness validaton to :name in 'tags' model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   has_many :tags_talks, dependent: :destroy, class_name: "TagTalk"
   has_many :talks, through: :tags_talks
+
+  validates :name, uniqueness: true
 end
