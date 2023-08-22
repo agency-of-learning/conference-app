7e74ba2c31819740d321b32bfd68110e13826fd4
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-22 14:55:14 -0400

    bug fix for notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,7 @@ class Notification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
 
-  after_create_commit -> { broadcast_prepend_to "notifications", target: "notifications_display"}
+  after_create_commit -> { broadcast_prepend_to "notifications_to_user_#{self.recipient_id}", target: "notifications_display"}
   
   after_create_commit do
     broadcast_update_to "broadcast_to_user_#{self.recipient_id}", 
@@ -12,14 +12,14 @@ class Notification < ApplicationRecord
   end
 
   after_create_commit do
-    broadcast_update_to "mobile", 
+    broadcast_update_to "mobile_to_user_#{self.recipient_id}", 
       target: "notifications_count_mobile", 
       partial: "notifications/count_mobile", 
       locals: { count: self.recipient.unread_notifications_count }
   end
 
   after_create_commit do
-    broadcast_update_to "menu", 
+    broadcast_update_to "menu_to_user_#{self.recipient_id}", 
       target: "notifications_count_menu", 
       partial: "notifications/count_menu", 
       locals: { count: self.recipient.unread_notifications_count }
