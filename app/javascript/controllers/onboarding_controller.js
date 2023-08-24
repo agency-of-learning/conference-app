a3af84f626c9f345c4f6ff0b6411cbde98531140
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-24 10:04:43 -0400

    removed unused code from controller

--- app/javascript/controllers/onboarding_controller.js
+++ app/javascript/controllers/onboarding_controller.js
@@ -3,12 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="onboarding"
 export default class extends Controller {
   static targets = ["image", "icon"]
-  connect() {
-    console.log("Im here GIRL!")
-    if (this.imageTarget.value){
-      this.iconTarget.style["filter"] = "drop-shadow(2px 4px 6px red)";
-    }
-  }
 
   upload(){
     this.iconTarget.style["filter"] = "drop-shadow(2px 4px 6px red)";
