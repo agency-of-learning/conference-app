fa019572345a03fc5e18f6d7d7fe4740c0791b44
SHAMI_TOMITA <shamitomita@gmail.com>
2023-08-24 16:42:02 -0400

    updates

---
+++ app/javascript/controllers/onboarding_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="onboarding"
+export default class extends Controller {
+  static targets = ["image", "icon"]
+
+  upload() {
+    this.iconTarget.style["filter"] = "drop-shadow(2px 4px 6px red)";
+  }
+}
