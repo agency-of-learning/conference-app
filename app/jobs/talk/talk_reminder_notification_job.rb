584f2e1627307cef19293440ce76845504f1996f
SHAMI_TOMITA <shamitomita@gmail.com>
2023-09-04 17:58:07 -0400

    job that might have unnecessary checks now

--- app/jobs/talk/talk_reminder_notification_job.rb
+++ app/jobs/talk/talk_reminder_notification_job.rb
@@ -3,7 +3,7 @@ class Talk::TalkReminderNotificationJob < ApplicationJob
 
   def perform(user, talk)
     return if !user.talks.include?(talk) #job shouldn't perform if user removed talk from their schedule
-    return if user.notifications.any? {|n| n.type == "Talk::ReminderNotification" && n.to_notification.talk == talk}
+    return if user.notifications.any? {|n| n.type == "Talk::ReminderNotification" && n.to_notification.talk == talk} #extra check? just in case?
 
     Talk::ReminderNotification.with(talk: talk).deliver(user)
   end
