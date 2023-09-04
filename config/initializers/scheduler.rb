f631112baf8a3069e3b5c3fa81628ead2e903b39
SHAMI_TOMITA <shamitomita@gmail.com>
2023-09-04 17:58:40 -0400

    scheduler and job simplification

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,12 +2,17 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '00 0 * * *' do
-    talks = Talk.happening_today 
-    talks.each {|talk| Talk::FanOutToUsersJob.perform_later(talk)}
-end 
+scheduler.every '10m' do
+    talks = Talk.happening_today.in_thirty_minutes.includes(:users) 
+    #talks.each {|talk| Talk::FanOutToUsersJob.perform_later(talk)} -> ORIGINAL
+    
+    # note: instead of checking each individual user in an enqueued job, 
+    #       will skip "FanOutToUsersJob" and only begin enqueueing jobs 
+    #       for users who have not been notified for that talk
 
-scheduler.every '20m' do
-    talks = Talk.happening_today 
-    talks.each {|talk| Talk::FanOutToUsersJob.perform_later(talk)}
+    talks.each do |talk|
+        talk.unnotified_users.each do |user| #ew nested loop, but it's already a nested loop anyways right?
+            Talk::ScheduleUserReminderJob.perform_later(user, talk)
+        end 
+    end 
 end 
