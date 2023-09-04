87c22eb152e5cfd0e6fbb01d0219f0bfc9924ce2
SHAMI_TOMITA <shamitomita@gmail.com>
2023-09-04 17:57:44 -0400

    potentally condemned job

--- app/jobs/talk/fan_out_to_users_job.rb
+++ app/jobs/talk/fan_out_to_users_job.rb
@@ -1,11 +1,13 @@
 #I get called in config/initializer/scheduler.rb
+#NOTE: should I even exist?
 class Talk::FanOutToUsersJob < ApplicationJob
   queue_as :default
 
   def perform(talk)
-    talk.users.each do |user|
+    talk.unnotified_users.each do |user|
       return if !user.talks.include?(talk) #in case a user removes a talk
-
+      return if user.notifications.any? {|n| n.type == "Talk::ReminderNotification" && n.to_notification.talk == talk}
+      
       Talk::ScheduleUserReminderJob.perform_later(user, talk)
     end 
   end
