685d6d5a61ebe5eccdd7c7c8a2eee70f400e8693
SHAMI_TOMITA <shamitomita@gmail.com>
2023-09-05 13:59:10 -0400

    BINGO

--- app/jobs/comments/post_to_slack_job.rb
+++ app/jobs/comments/post_to_slack_job.rb
@@ -1,16 +1,17 @@
 class Comments::PostToSlackJob < ApplicationJob
   queue_as :default
 
-  def perform(comment)
-    HTTParty.post(endpoint, body: body(comment), headers: headers)
+  def perform(comment_body)
+    @comment_body = comment_body
+    HTTParty.post(endpoint, body: body, headers: headers)
   end
 
   def endpoint
     "https://yov8r.hatchboxapp.com/webhooks/incoming/rails_world_feedback_webhooks"
   end
 
-  def body(comment)
-    { comment: comment }.to_json
+  def body
+    { comment: { comment: @comment_body } }.to_json
   end
 
   def headers
