68860a007ef9b91e731d620345c1d691bbd822ee
SHAMI_TOMITA <shamitomita@gmail.com>
2023-09-06 15:26:56 -0400

    onboarding updates

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -2,29 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="preview"
 export default class extends Controller {
-  static targets = ["input", "dummy", "preview"]
+  static targets = ["input", "dummy", "preview", "error"]
 
   connect(){
     console.log("Hello!", this.element)
   }
   preview() {
-    // TODO: this
+    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png', 'image/heic'];
+
     let input = this.inputTarget;
     let preview = this.previewTarget;
     let dummy = this.dummyTarget
     let file = input.files[0];
+    let error = this.errorTarget;
     let reader = new FileReader();
+
     console.log(input, preview, file)
+    error.style.visibility = "hidden"
     reader.onloadend = function () {
       preview.src = reader.result;
     };
 
-    if (file) {
+    if (file && acceptedImageTypes.includes(file['type'])) {
       dummy.style.display = "none"
       preview.style.display = "flex"
       reader.readAsDataURL(file);
     } else {
-      preview.src = "";
+      console.log("Not an image file!")
+      error.style.visibility = "visible"
     }
   }
 }
