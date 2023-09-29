b84e9f8f532775788295762c578634a2c9010e04
Cody Norman <wcodynorman@gmail.com>
2023-09-29 12:26:18 -0600

    set default from on staging

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@railsworld.agencyoflearning.com", to: -> { @recipient.email }
+  if Rails.env.production?
+    default from: "no-reply@railsworld.agencyoflearning.com", to: -> { @recipient.email }
+  elsif Rails.env.staging?
+    default from: "no-reply@mail.montibuslabs.com", to: -> { @recipient.email }
+  end
+
 
   before_action :set_recipient
 
