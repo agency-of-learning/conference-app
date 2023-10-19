7562cd3d3008c50e0f67f2b3fb1776d44ee8821e
Cody Norman <wcodynorman@gmail.com>
2023-10-19 09:55:15 -0600

    run rails app update task

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-require 'rails_autolink'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,17 +11,17 @@ module RailsWorldConf
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-
-    config.time_zone = "Europe/Amsterdam"
-    
-    #For testing or QA purposes, make sure to switch time_zone to your current one
-    #config.time_zone = 'Eastern Time (US & Canada)'
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.active_job.queue_adapter = :sidekiq
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,21 +1,12 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
-    Bullet.bullet_logger = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
-  end
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.enable_reloading = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -42,7 +33,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_controller.default_url_options = { host: "localhost:3000", port: 3000 }
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
@@ -66,6 +56,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -78,11 +71,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.delivery_method = :letter_opener_web
-
-  config.action_mailer.perform_deliveries = true
-  
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
-  
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = true
 end
