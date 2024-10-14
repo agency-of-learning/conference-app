e27c27741bd84b4482ed7a03f66066e676b620ac
Eric Halverson <ericleifhalverson@gmail.com>
2024-10-14 16:04:41 -0700

    Refactor talks_controller and tabs_by_day view

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -4,17 +4,9 @@ class TalksController < ApplicationController
 
   # GET /talks
   def index
-    conference_start_date = Date.new(2023, 10, 18) # Start of conference
-    conference_end_date = Date.new(2023, 10, 20) # End of conference
-
-    talks_by_day = Talk.includes(:speakers, :tags)
-    .where(start_time: conference_start_date.beginning_of_day..conference_end_date.end_of_day)
+    @talks_by_day = Talk.includes(:speakers, :tags)
     .order(:start_time)
     .group_by { |talk| talk.start_time.to_date }
-
-    @day_one_talks = talks_by_day[conference_start_date] || []
-    @day_two_talks = talks_by_day[conference_start_date + 1.day] || []
-    @day_three_talks = talks_by_day[conference_start_date + 2.day] || []
   end
 
   # GET /talks/1
